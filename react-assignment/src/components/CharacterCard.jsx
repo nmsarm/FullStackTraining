3253547d2c0bf68e18ab0e66156b96d2e1a15828
nmsarm <daneahsarmiento@gmail.com>
2025-02-15 02:13:50 +0800

    code refractor - mapping of character details

--- react-assignment/src/components/CharacterCard.jsx
+++ react-assignment/src/components/CharacterCard.jsx
@@ -6,41 +6,31 @@ const Card = ({ character }) => {
     const options = { year: "numeric", month: "long", day: "numeric"}
     return new Date(dateStr).toLocaleDateString(undefined, options)
   }
+  const characterData = [
+    { label: "ID", value: character.id },
+    { label: "Status", value: character.status },
+    { label: "Species", value: character.species },
+    { label: "Gender", value: character.gender },
+    { label: "Origin", value: character.origin.name },
+    { label: "Last Location", value: character.location.name }
+  ];
 
   return (
     <div className="card p-0" style={{width: "20rem"}}>
-      <img src={character.image} className="card-img-top " alt="character-image" />
+      <img src={character.image} className="card-img-top" alt="character-image" />
       <div className="card-body">
         <h5 className="card-title text-center">{character.name}</h5>
-        <p className="card-text m-0 text-center text-muted">Created {formatDate(dateStr)} </p>
+        <p className="card-text m-0 text-center text-muted">Created {formatDate(dateStr)}</p>
         <hr />
-        <div className="row">
-          <div className="col text-start"><p className="card-text m-0 fw-bold">ID:</p></div>
-          <div className="col text-end"><p className="card-text m-0">{character.id}</p></div>
-        </div>
-        <div className="row">
-          <div className="col text-start"><p className="card-text m-0 fw-bold">Status:</p></div>
-          <div className="col text-end"><p className="card-text m-0">{character.status}</p></div>
-        </div>
-        <div className="row">
-          <div className="col text-start"><p className="card-text m-0 fw-bold">Species:</p></div>
-          <div className="col text-end"><p className="card-text m-0">{character.species}</p></div>
-        </div>
-        <div className="row">
-          <div className="col text-start"><p className="card-text m-0 fw-bold">Gender:</p></div>
-          <div className="col text-end"><p className="card-text m-0">{character.gender}</p></div>
-        </div>
-        <div className="row">
-          <div className="col text-start"><p className="card-text m-0 fw-bold">Origin:</p></div>
-          <div className="col text-end"><p className="card-text m-0">{character.origin.name}</p></div>
-        </div>
-        <div className="row">
-          <div className="col text-start"><p className="card-text m-0 fw-bold">Last Location:</p></div>
-          <div className="col text-end"><p className="card-text m-0">{character.location.name}</p></div>
-        </div>
+        {characterData.map((data, index) => (
+          <div className="row" key={index}>
+            <div className="col text-start"><p className="card-text m-0 fw-bold">{data.label}:</p></div>
+            <div className="col text-end"><p className="card-text m-0">{data.value}</p></div>
+          </div>
+        ))}
       </div>
     </div>
-  ) 
+  );
 }
 
 export default Card
